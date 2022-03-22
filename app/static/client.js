9880bca4a97e3be2716b7b26d76a3ee6dcb19a2e
falul <falul@walla.co.il>
2022-03-23 00:18:20 +0200

    added cropper, new results background

--- app/static/client.js
+++ app/static/client.js
@@ -1,21 +1,44 @@
+function dataURLtoFile(dataurl, filename) {
+ 
+  var arr = dataurl.split(','),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]), 
+      n = bstr.length, 
+      u8arr = new Uint8Array(n);
+      
+  while(n--){
+      u8arr[n] = bstr.charCodeAt(n);
+  }
+  
+  return new File([u8arr], filename, {type:mime});
+}
+
 var el = x => document.getElementById(x);
 
 function showPicker() {
   el("file-input").click();
+  el("result-label").classList.add('no-display');
+
 }
 
 function showPicked(input) {
   el("upload-label").innerHTML = input.files[0].name;
   var reader = new FileReader();
   reader.onload = function(e) {
-    el("image-picked").src = e.target.result;
-    el("image-picked").className = "";
+    // el("image-picked").src = e.target.result;
+    // el("image-picked").className = "";
+    crop(e.target.result);
+    // console.log(e.target.result)
   };
   reader.readAsDataURL(input.files[0]);
 }
 
-function analyze() {
-  var uploadFiles = el("file-input").files;
+
+
+
+async function analyze() {
+  var croppedFile = await cropSave()
+  var uploadFiles = [dataURLtoFile(croppedFile,'image.jpg')];
   if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
 
   el("analyze-button").innerHTML = "Analyzing...";
@@ -30,8 +53,9 @@ function analyze() {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
       el("result-label").innerHTML = `Result: Probably ${response["result"]}`;
+      el("result-label").classList.remove('no-display');
     }
-    el("analyze-button").innerHTML = "Analyze";
+    el("analyze-button").innerHTML = "Inspect";
   };
 
   var fileData = new FormData();
